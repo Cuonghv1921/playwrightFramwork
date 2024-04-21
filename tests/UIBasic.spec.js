c24327ed498bf21a4e561e0640e2beb9ba8dbc59
CuongHa <cuonghv1921@gmail.com>
2024-04-21 22:44:47 +0700

    update E2E checkout flow

--- tests/UIBasic.spec.js
+++ tests/UIBasic.spec.js
@@ -1,4 +1,4 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 test('First Playwight test with declare test context', async ({browser}) => {
     const context = await browser.newContext();
@@ -7,5 +7,32 @@ test('First Playwight test with declare test context', async ({browser}) => {
 });
 
 test.only('First Playwight test with default test context', async ({page}) => {
-    await page.goto("https://rahulshettyacademy.com/client");
+    await page.goto("https://vnexpress.net/");
+    await expect(page).toHaveTitle("VnExpress - Báo tiếng Việt nhiều người xem nhất");
+    await page.locator("xpath=//a[@class='logo']").click();
 });
+
+test('@Web Client App login', async ({ page }) => {
+    //js file- Login js, DashboardPage
+    const email = "cuonghv64@wru.vn";
+    const pwd = "Minhhue1921";
+    const productName = 'zara coat 3';
+    const products = page.locator(".card-body");
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill(email);
+    await page.locator("#userPassword").fill(pwd);
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles); 
+  
+ })
+
+ test('Test dropdown and radio button', async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("a[routerlink='/auth/register']").click();
+    await page.locator("select[formcontrolname='occupation']").selectOption("3: Engineer");
+    await page.locator("input[value='Male']").click();  
+    // await page.pause();
+ })
